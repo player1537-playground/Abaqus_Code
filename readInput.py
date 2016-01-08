b069d6d5f3238fe0c49fe8fc0d2cb4e8c7be1e48
Michael Haines <m.haines333@gmail.com>
2016-01-08 13:05:30 -0500

    Seperated Abaqus code from standard Python code and have both working independently.

--- readInput.py
+++ readInput.py
@@ -1,12 +1,6 @@
 __author__ = 'michael'
 
-# from sys import path
-# path.append('C:\Python27\Lib\site-packages')
-
-import os
-from pathos.multiprocessing import ProcessingPool
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
+import multiprocessing
 
 '''from abaqus import *
 from abaqusConstants import *
@@ -17,199 +11,169 @@ import load
 import interaction'''
 
 
-class GetElements():
+def getnodes(givenInstances, inputfile):
 
-    def __init__(self, inputfile, giveninstances, givenParts, model, numlayer, startingLayer,):
-        self.inputfile = inputfile
-        self.givenInstances = giveninstances
-        self.givenParts = givenParts
-        self.model = model
-        self.numlayer = numlayer
-        self.startLayer = startingLayer
+    import re
+    # import matplotlib.pyplot as plt
+    # from mpl_toolkits.mplot3d import Axes3D
 
-    def getnodes(self, givenInstances):
+    print "locating nodes"
+    model = givenInstances
+    filehandle = open(inputfile, 'r')
+    with filehandle as f:
+        content = f.readlines()
+    #   for model in givenInstances: #was self.givenInstaces
+        nodes = []
+        elements = []
+        count = 0
+        start = 0
+        end = 0
+        for index, line in enumerate(content):
+            if line.find(model) >= 0:
+                count = index
+                break
 
-        import re
-        import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-        output_list = None
+        for line in content[count:]:
+            if "*Node" in line:
+                count += 1
+                start = count
+            elif '*Element' in line:
+                end = count
+                break
+            else:
+                count += 1
 
-        print "locating nodes"
-        model = givenInstances
-        filehandle = open(self.inputfile, 'r')
-        with filehandle as f:
-            content = f.readlines()
-        #   for model in givenInstances: #was self.givenInstaces
-            nodes = []
-            elements = []
-            count = 0
-            start = 0
-            end = 0
-            for index, line in enumerate(content):
-                if line.find(model) >= 0:
-                    count = index
-                    break
-
-            for line in content[count:]:
-                if "*Node" in line:
-                    count += 1
-                    start = count
-                elif '*Element' in line:
-                    end = count
-                    break
-                else:
-                    count += 1
-
-            for i in range(start, end):
-                nodes.append(re.findall(r'[+-]?\d+\.*\d*', content[i]))
+        for i in range(start, end):
+            nodes.append(re.findall(r'[+-]?\d+\.*\d*', content[i]))
 
 
-            for line in content[count:]:
-                if "*Element" in line:
-                    count += 1
-                    start = count
-                elif '*Nset,' in line:
-                    end = count
-                    break
-                else:
-                    count += 1
+        for line in content[count:]:
+            if "*Element" in line:
+                count += 1
+                start = count
+            elif '*Nset,' in line:
+                end = count
+                break
+            else:
+                count += 1
 
-            for i in range(start, end):
-                elements.append(re.findall(r'[+-]?\d+\.*\d*', content[i]))
+        for i in range(start, end):
+            elements.append(re.findall(r'[+-]?\d+\.*\d*', content[i]))
 
-            elements = [[float(f) for f in row] for row in elements]
-            nodes = [[float(f) for f in row] for row in nodes]
-            cool = sorted(nodes, key=lambda x: x[2],)
-            # print elements
-            # print cool
-            last_layer = min(nodes, key=lambda x: x[3],)[3] #change axis from 2
-            list_of_layers = []
+        elements = [[float(f) for f in row] for row in elements]
+        nodes = [[float(f) for f in row] for row in nodes]
+        cool = sorted(nodes, key=lambda x: x[2],)
+        # print elements
+        # print cool
+        last_layer = min(nodes, key=lambda x: x[3],)[3] #change axis from 2
+        list_of_layers = []
 
-            y = [i[3] for i in nodes] # change axis from 2
+        y = [i[3] for i in nodes] # change axis from 2
 
-            seen = set()
-            seen_add = seen.add
-            layers = [x for x in y if not (x in seen or seen_add(x))]
-            layers = layers[::-1]
-            print layers[1:]
+        seen = set()
+        seen_add = seen.add
+        layers = [x for x in y if not (x in seen or seen_add(x))]
+        layers = layers[::-1]
+        print layers[1:]
 
-            for layer in layers[1:]:
-                x = []
-                y = []
-                z = []
-                node_numbers = []
-                element_numbers = []
+        for layer in layers[1:]:
+            x = []
+            y = []
+            z = []
+            node_numbers = []
+            element_numbers = []
 
-                print layer
-                print last_layer
+            print layer
+            print last_layer
 
-                for i in nodes:
-                    if i[3] <= layer and i[3] >= last_layer: # change axis from 2
-                        node_numbers.append((i[0]))
-                        x.append(i[1])
-                        y.append(i[2])
-                        z.append(i[3])
+            for i in nodes:
+                if i[3] <= layer and i[3] >= last_layer: # change axis from 2
+                    node_numbers.append((i[0]))
+                    x.append(i[1])
+                    y.append(i[2])
+                    z.append(i[3])
 
-                #print y
-                last_layer = layer
+            #print y
+            last_layer = layer
 
-                for i in elements:
-                    element_numbers.append(i[0])
+            for i in elements:
+                element_numbers.append(i[0])
 
-                elements_to_write = []
+            elements_to_write = []
 
-                print("gathering Elements")
-                for index, elemnum  in enumerate(element_numbers):
-                    number_of_nodes = 0
-                    for node in node_numbers:
-                        if node in elements[index][1:8]:
-                            number_of_nodes += 1
+            print("gathering Elements")
+            for index, elemnum  in enumerate(element_numbers):
+                number_of_nodes = 0
+                for node in node_numbers:
+                    if node in elements[index][1:8]:
+                        number_of_nodes += 1
 
-                    if number_of_nodes == 7:
-                        elements_to_write.append(elemnum)
-                print("Converting array")
-                elements_to_write = [int(i) for i in elements_to_write]
-                print elements_to_write
-                list_of_layers.append(elements_to_write)
+                if number_of_nodes == 7:
+                    elements_to_write.append(elemnum)
+            print("Converting array")
+            elements_to_write = [int(i) for i in elements_to_write]
+            print elements_to_write
+            list_of_layers.append(elements_to_write)
 
-                fig = plt.figure()
-                ax = fig.add_subplot(111, projection='3d')
-                ax.scatter(x, y, z, zdir='y')
-                #ax.set_xlim(-.5,.25)
-                #ax.set_ylim(-.25,.5)
-                #ax.set_zlim(-.25,.25)
-                # plt.show()
+            '''fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(x, y, z, zdir='y')
+            #ax.set_xlim(-.5,.25)
+            #ax.set_ylim(-.25,.5)
+            #ax.set_zlim(-.25,.25)
+            # plt.show()'''
 
-            list_of_layers = [[int(x) for x in row] for row in list_of_layers]
-            print list_of_layers
-
-            first_layer = 1
-            temp_list = []
-            for i in list_of_layers[first_layer:]:
-                for k in i:
-                    temp_list.append(k)
-            print temp_list
-            # print model
-            output_list = list_of_layers
-            '''boxElements = self.model.rootAssembly.instances[model].elements
-            boxSet = boxElements.sequenceFromLabels(temp_list)
-            BoxSectionSet = self.model.rootAssembly.Set(name=model + '_BoxSectionSet_0', elements=boxSet)
-            boxSet = boxElements.sequenceFromLabels(list_of_layers[first_layer-1])
-            boxSurface = self.model.rootAssembly.Surface(name=model + '_BoxSectionSurface_0', face6Elements=boxSet)
-            count = 1
-            for i in list_of_layers[first_layer:]:
-                boxSet = boxElements.sequenceFromLabels(i)
-                BoxSectionSet = self.model.rootAssembly.Set(name=model + '_BoxSectionSet_' + str(count), elements=boxSet)
-                boxSurface = self.model.rootAssembly.Surface(name=model + '_BoxSectionSurface_' + str(count),
-                                                         face6Elements=boxSet)'''
+        list_of_layers = [[int(x) for x in row] for row in list_of_layers]
+        print list_of_layers
 
         filehandle.close()
         filehandle = open(str(model) + "_layer", 'w')
         # output_list = [[str(x) for x in row] for row in output_list]
-        filehandle.write(str(output_list))
+        for i in list_of_layers:
+            filehandle.write(str(i) + '\n')
+        filehandle.close()
+
+def getlayers():
+
+    for i in range(max(numlayer)):
+        layermerge = []
+        for index in range(len(givenInstances)):
+            if i <= numlayer[index]:
+                layermerge.append(model_attribute.rootAssembly.sets[givenInstances[index] + '_BoxSectionSet_' +
+                                                                    str(i + startLayer[index])])
+        BoxSectionSet = model_attribute.rootAssembly.SetByBoolean(name="MergedSet_" + str(i), sets=layermerge)
+        if i == 0:
+            model_attribute.HeatTransferStep(name='test_' + str(i), previous='Initial', timePeriod=3, initialInc=.001,
+                                             minInc=0.00001, maxInc=1, deltmx=100.0)
+            model_attribute.ModelChange(name='Model_Change_Test_' + str(i), createStepName='test_' + str(i),
+                                        regionType=ELEMENTS, region=BoxSectionSet)
+            model_attribute.FieldOutputRequest(name='F-Output-layer-' + str(i), createStepName='test_' + str(i),
+                                               variables=('NT', 'HFL', 'RFL'))
 
 
-    def getlayers(self):
+        else:
+            model_attribute.HeatTransferStep(name='test_' + str(i), previous='test_' + str(i - 1),
+                                             timePeriod=3, initialInc=.001, minInc=0.00001, maxInc=1, deltmx=100.0)
+            model_attribute.ModelChange(name='Model_Change_Test_' + str(i), createStepName='test_' + str(i),
+                                        regionType=ELEMENTS, region=BoxSectionSet, activeInStep=True)
+            model_attribute.FieldOutputRequest(name='F-Output-layer-' + str(i), createStepName='test_' + str(i),
+                                               variables=('NT', 'HFL', 'RFL'))
 
-        for i in range(max(self.numlayer)):
-            layermerge = []
-            for index in range(len(self.givenInstances)):
-                if i <= self.numlayer[index]:
-                    layermerge.append(self.model.rootAssembly.sets[self.givenInstances[index] + '_BoxSectionSet_' +
-                                                                   str(i + self.startLayer[index])])
-            BoxSectionSet = self.model.rootAssembly.SetByBoolean(name="MergedSet_" + str(i), sets=layermerge)
-            if i == 0:
-                self.model.HeatTransferStep(name='test_' + str(i), previous='Initial',timePeriod=3, initialInc=.001,
-                                            minInc=0.00001, maxInc=1, deltmx=100.0)
-                self.model.ModelChange(name='Model_Change_Test_' + str(i), createStepName='test_' + str(i),
-                                       regionType=ELEMENTS, region=BoxSectionSet)
-                self.model.FieldOutputRequest(name='F-Output-layer-' + str(i),  createStepName='test_' + str(i),
-                                              variables=('NT', 'HFL', 'RFL'))
+    for i in range(max(numlayer)):
+        surfacemerge = []
+        for index in range(len(givenParts)):
+            if i <= numlayer[index]:
+                surfacemerge.append(model_attribute.rootAssembly.surfaces[givenParts[index] + '_BoxSectionSurface_' +
+                                                                          str(i)])
 
+        boxSurface = model_attribute.rootAssembly.SurfaceByBoolean(name="MergedSurface_" + str(i), surfaces=surfacemerge)
+        if i == 0:
+            model_attribute.SurfaceHeatFlux(name='Heat_Flux_' + str(i), createStepName='test_' + str(i),
+                                            region=boxSurface, magnitude=10000000.0, amplitude='Heat_Transfer_Amp')
 
-            else:
-                self.model.HeatTransferStep(name='test_' + str(i), previous='test_' + str(i-1),
-                                            timePeriod=3, initialInc=.001, minInc=0.00001, maxInc=1, deltmx=100.0)
-                self.model.ModelChange(name='Model_Change_Test_' + str(i), createStepName='test_' + str(i),
-                                       regionType=ELEMENTS, region=BoxSectionSet, activeInStep=True)
-                self.model.FieldOutputRequest(name='F-Output-layer-' + str(i),  createStepName='test_' + str(i),
-                                              variables=('NT', 'HFL', 'RFL'))
-
-        for i in range(max(self.numlayer)):
-            surfacemerge = []
-            for index in range(len(self.givenParts)):
-                if i <= self.numlayer[index]:
-                    surfacemerge.append(self.model.rootAssembly.surfaces[self.givenParts[index] + '_BoxSectionSurface_' +
-                                                                     str(i)])
-
-            boxSurface = self.model.rootAssembly.SurfaceByBoolean(name="MergedSurface_" + str(i), surfaces=surfacemerge)
-            if i == 0:
-                self.model.SurfaceHeatFlux(name='Heat_Flux_' + str(i), createStepName='test_' + str(i),
-                                           region=boxSurface, magnitude=10000000.0, amplitude='Heat_Transfer_Amp')
-
-            else:
-                self.model.SurfaceHeatFlux(name='Heat_Flux_' + str(i), createStepName='test_' + str(i),
-                                           region=boxSurface, magnitude=10000000.0, amplitude='Heat_Transfer_Amp')
+        else:
+            model_attribute.SurfaceHeatFlux(name='Heat_Flux_' + str(i), createStepName='test_' + str(i),
+                                            region=boxSurface, magnitude=10000000.0, amplitude='Heat_Transfer_Amp')
 
 
 '''def _pickle_method(method):
@@ -230,28 +194,31 @@ def _unpickle_method(func_name, obj, cls):
     return func.__get__(obj, cls)'''
 
 
-if __name__== "__main__":
+# copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+# mymodel = mdb.models['Simplified Arcam model']
 
-    # copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+if __name__ == '__main__':
+
     # mymodel = mdb.models['Simplified Arcam model']
 
-    os.chdir("C:\Users\Michael Haines\PycharmProjects\Abaqus_Code")
-
-    filehandle = open("Heat_Transfer_Test_1.inp", 'r')
-    content = None
-    with filehandle as f:
-            content = f.readlines()
-    filehandle.close()
-
-    grabnodes =GetElements("Heat_Transfer_Test_1.inp",
-                           ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
-                            'test_sample-1-rad-2', 'test_sample-1-rad-4'],
-                           ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
-                            'test_sample-1-rad-2', 'test_sample-1-rad-4'], 'mymodel', [20, 20, 20, 20, 20, 20],
-                           [0, 0, 0, 0, 0, 0])
+    inputfile = "Heat_Transfer_Test_1.inp"
+    givenInstances = ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
+                      'test_sample-1-rad-2', 'test_sample-1-rad-4']
+    givenParts = ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
+                  'test_sample-1-rad-2', 'test_sample-1-rad-4']
+    model_attribute = 'mymodel'
+    numlayer = [20, 20, 20, 20, 20, 20]
+    startLayer = [0, 0, 0, 0, 0, 0]
 
     items = ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
                             'test_sample-1-rad-2', 'test_sample-1-rad-4']
 
-    p = ProcessingPool(6)
-    p.map(GetElements.getnodes, [grabnodes]*6, items)
+    pool = multiprocessing.Pool() #use all available cores, otherwise specify the number you want as an argument
+    for i in items:
+        pool.apply_async(getnodes, args=(i, inputfile))
+    pool.close()
+    pool.join()
+
+    # getlayers()
+# p = ProcessingPool(6)
+# p.map(getnodes, [grabnodes]*6, items)
--- toAbaqus.py
+++ toAbaqus.py
@@ -1,16 +1,18 @@
 
-import subprocess
 
-'''from abaqus import *
+import re
+import os
+
+from abaqus import *
 from abaqusConstants import *
 import part
 import assembly
 import step
 import load
-import interaction'''
+import interaction
 
 
-class AbaqusLayers():
+class AbaqusLayers:
 
     def __init__(self, inputfile, giveninstances, givenParts, model, numlayer, startingLayer,):
         self.inputfile = inputfile
@@ -20,6 +22,44 @@ class AbaqusLayers():
         self.numlayer = numlayer
         self.startLayer = startingLayer
 
+    def getregions(self):
+
+        abaqus_directory = os.getcwd()
+        os.chdir('C:\Users\Michael Haines\PycharmProjects\Abaqus_Code')
+
+        for model in self.givenInstances:
+
+            list_of_layers = []
+
+            filehandle = open(model + '_layer', 'r')
+            with filehandle as f:
+                content = f.readlines()
+                temp = []
+
+                for line in content:
+                    # temp.append(re.findall(r'[+-]?\d+\.*\d*', line))
+                    list_of_layers.append(re.findall(r'[+-]?\d+\.*\d*', line))
+
+            list_of_layers = [[int(x) for x in row] for row in list_of_layers]
+            first_layer = 1
+
+            temp_list = []
+            for i in list_of_layers[first_layer:]:
+                for k in i:
+                    temp_list.append(k)
+
+            boxElements = self.model.rootAssembly.instances[model].elements
+            boxSet = boxElements.sequenceFromLabels(temp_list)
+            BoxSectionSet = self.model.rootAssembly.Set(name=model + '_BoxSectionSet_0', elements=boxSet)
+            boxSet = boxElements.sequenceFromLabels(list_of_layers[first_layer-1])
+            boxSurface = self.model.rootAssembly.Surface(name=model + '_BoxSectionSurface_0', face2Elements=boxSet)
+            count = 1
+            for i in list_of_layers[first_layer:]:
+                boxSet = boxElements.sequenceFromLabels(i)
+                BoxSectionSet = self.model.rootAssembly.Set(name=model + '_BoxSectionSet_' + str(count), elements=boxSet)
+                boxSurface = self.model.rootAssembly.Surface(name=model + '_BoxSectionSurface_' + str(count),
+                                                             face2Elements=boxSet)
+                count += 1
 
     def getlayers(self):
 
@@ -31,8 +71,8 @@ class AbaqusLayers():
                                                                    str(i + self.startLayer[index])])
             BoxSectionSet = self.model.rootAssembly.SetByBoolean(name="MergedSet_" + str(i), sets=layermerge)
             if i == 0:
-                self.model.HeatTransferStep(name='test_' + str(i), previous='Initial',timePeriod=3, initialInc=.001,
-                                            minInc=0.00001, maxInc=1, deltmx=100.0)
+                self.model.HeatTransferStep(name='test_' + str(i), previous='Initial',timePeriod=60, initialInc=.001,
+                                            minInc=0.00001, maxInc=1, maxNumInc=500, deltmx=100.0)
                 self.model.ModelChange(name='Model_Change_Test_' + str(i), createStepName='test_' + str(i),
                                        regionType=ELEMENTS, region=BoxSectionSet)
                 self.model.FieldOutputRequest(name='F-Output-layer-' + str(i),  createStepName='test_' + str(i),
@@ -41,7 +81,7 @@ class AbaqusLayers():
 
             else:
                 self.model.HeatTransferStep(name='test_' + str(i), previous='test_' + str(i-1),
-                                            timePeriod=3, initialInc=.001, minInc=0.00001, maxInc=1, deltmx=100.0)
+                                            timePeriod=60, initialInc=.001, minInc=0.00001, maxInc=1, maxNumInc=500, deltmx=100.0)
                 self.model.ModelChange(name='Model_Change_Test_' + str(i), createStepName='test_' + str(i),
                                        regionType=ELEMENTS, region=BoxSectionSet, activeInStep=True)
                 self.model.FieldOutputRequest(name='F-Output-layer-' + str(i),  createStepName='test_' + str(i),
@@ -57,10 +97,24 @@ class AbaqusLayers():
             boxSurface = self.model.rootAssembly.SurfaceByBoolean(name="MergedSurface_" + str(i), surfaces=surfacemerge)
             if i == 0:
                 self.model.SurfaceHeatFlux(name='Heat_Flux_' + str(i), createStepName='test_' + str(i),
-                                           region=boxSurface, magnitude=10000000.0, amplitude='Heat_Transfer_Amp')
+                                           region=boxSurface, magnitude=500000000.0, amplitude='Heat_Transfer_Amp')
 
             else:
                 self.model.SurfaceHeatFlux(name='Heat_Flux_' + str(i), createStepName='test_' + str(i),
-                                           region=boxSurface, magnitude=10000000.0, amplitude='Heat_Transfer_Amp')
+                                           region=boxSurface, magnitude=500000000.0, amplitude='Heat_Transfer_Amp')
 
 if __name__ == "__main__":
+
+    abaqus_directory = os.getcwd()
+    mymodel = mdb.models['Simplified Arcam model']
+
+    grabnodes = AbaqusLayers("Heat_Transfer_Test_1.inp",
+                           ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
+                            'test_sample-1-rad-2', 'test_sample-1-rad-4'],
+                           ['test_sample-1', 'test_sample-1-lin-2-1', 'test_sample-1-lin-3-1', 'test_sample-1-lin-4-1',
+                            'test_sample-1-rad-2', 'test_sample-1-rad-4'], mymodel, [20, 20, 20, 20, 20, 20],
+                           [0, 0, 0, 0, 0, 0])
+
+    grabnodes.getregions()
+    grabnodes.getlayers()
+    os.chdir(abaqus_directory)
